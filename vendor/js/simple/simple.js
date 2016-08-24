63248cc4f078ac948beb27dbc00e0073f742e820
Bryan T. Meyers <bmeyers@datadrake.com>
2016-08-23 22:00:46 -0400

    trying to fix vertical alignment

--- vendor/js/simple/simple.js
+++ vendor/js/simple/simple.js
@@ -1,3 +1,3 @@
 //requirejs(["./modules/delete"], function(util) {});
 //requirejs(["./modules/forms"], function(util) {});
-requirejs(["./modules/top"], function(util) {});
+requirejs(["vendor/js/simple/modules/top"], function(util) {});
